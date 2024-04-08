87a1482e8a028f06825fb240582fc7e3683dbfdd
Juan12341234 <juansebastiantombepatino@gmail.com>
2024-04-08 13:47:30 -0500

    Agregación de login

---
+++ Model/ventasModel.js
@@ -0,0 +1,31 @@
+const Ventas = [
+    {
+        id: 1,
+        agendamiento: 2,
+        diagnostico: 'Resfriado común',
+        fecha: '2024-04-05',
+        hora: '10:00',
+        precio: 50,
+        metodoPago: 'Tarjeta de crédito'
+    },
+    {
+        id: 2,
+        agendamiento: 1,
+        diagnostico: 'Estado de salud óptimo',
+        fecha: '2024-04-10',
+        hora: '15:30',
+        precio: 80,
+        metodoPago: 'Efectivo'
+    },
+    {
+        id: 3,
+        agendamiento: 3,
+        diagnostico: 'Extracción de uña encarnada',
+        fecha: '2024-04-15',
+        hora: '08:00',
+        precio: 120,
+        metodoPago: 'Transferencia bancaria'
+    },
+];
+
+module.exports = Ventas;
--- index.js
+++ index.js
@@ -2,36 +2,15 @@ const express = require("express");
 
 const app = express();
 
-const Ventas = [
-    {
-        id: 1,
-        agendamiento: 2,
-        diagnostico: 'Resfriado común',
-        fecha: '2024-04-05',
-        hora: '10:00',
-        precio: 50,
-        metodoPago: 'Tarjeta de crédito'
-    },
-    {
-        id: 2,
-        agendamiento: 1,
-        diagnostico: 'Estado de salud óptimo',
-        fecha: '2024-04-10',
-        hora: '15:30',
-        precio: 80,
-        metodoPago: 'Efectivo'
-    },
-    {
-        id: 3,
-        agendamiento: 3,
-        diagnostico: 'Extracción de uña encarnada',
-        fecha: '2024-04-15',
-        hora: '08:00',
-        precio: 120,
-        metodoPago: 'Transferencia bancaria'
-    },
-];
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const Ventas = require('./Model/ventasModel');
+const User = require('./Model/userModel');
+
+app.use("/login", function (req, res) {
+    return res.json(User);
+});
 
 app.use("/ventas/:id", function (req, res) {
     const {id} = req.params;
